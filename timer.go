8a606f271586f41279b2b261527d5ca8d894f0e7
Nicolas Knoebber <knoebber@gmail.com>
2020-01-04 17:34:24 -0600

    Add keyboard driven tview timers as proof of concept

--- timer.go
+++ timer.go
@@ -7,24 +7,39 @@ import (
 	"github.com/rivo/tview"
 )
 
-// Measure times to the tenth of a second.
+// Accuracy of time measurment.
 const (
 	refreshInterval = time.Second / 10
-	timeFormat      = "15:04:05.0"
 )
 
 var (
-	timer *tview.Box
+	runStart   time.Time
+	splitStart time.Time
 )
 
-func drawTime(screen tcell.Screen, x int, y int, width int, height int) (int, int, int, int) {
-	timeStr := time.Now().Format(timeFormat)
-	tview.Print(screen, timeStr, x, height/2, width, tview.AlignCenter, tcell.ColorLime)
+func elapsedStr(start time.Time) string {
+	elapsed := time.Since(start)
+	elapsed = elapsed - (elapsed % refreshInterval)
+	return elapsed.String()
+}
+
+func drawRunTime(screen tcell.Screen, x, y, width, height int) (int, int, int, int) {
+	tview.Print(screen, elapsedStr(runStart), x, height/2, width, tview.AlignCenter, tcell.ColorLime)
 	return 0, 0, 0, 0
+}
+
+func drawSplitTime(screen tcell.Screen, x, y, width, height int) (int, int, int, int) {
+	tview.Print(screen, elapsedStr(splitStart), x, height/2, width, tview.AlignCenter, tcell.ColorLime)
+	return 0, 0, 0, 0
+}
+
+func startSplit() {
+	splitStart = time.Now()
 }
 
 func refresh() {
 	tick := time.NewTicker(refreshInterval)
+
 	for {
 		select {
 		case <-tick.C:
@@ -33,12 +48,63 @@ func refresh() {
 	}
 }
 
-func startTimer() error {
-	timer = tview.NewBox().SetDrawFunc(drawTime)
-	if err := app.SetRoot(timer, true).Run(); err != nil {
-		panic(err)
+// Returning nil stops the input from propagating.
+func inputHandler(event *tcell.EventKey) *tcell.EventKey {
+	switch event.Rune() {
+	case ' ':
+		startSplit()
+		return nil
 	}
 
+	switch event.Key() {
+	case tcell.KeyEnter:
+		startSplit()
+		return nil
+	}
+	return event
+}
+
+/*
+Intended layout:
+Layout:
+| BoB 1    | <comparison segment> | <comparison split> |
+| PSS 2    |                   +1 |               2:44 |
+| Whomps 9 |                   -3 |               8:33 |
+( highlight current row)
+---
+- Total Time :: xx:xx
+- Segment :: xx
+- Gold :: xx
+- Time Save :: xx
+- Best Possible Time :: xx
+- Sum of Gold :: xx
+*/
+func startTimer() error {
+	var (
+	// totalTimeBox  *tview.Box
+	// totalDuration time.Duration
+	// splitDuration time.Duration
+	)
+	// Goal:
+	// Implement two timers:
+	// One for total time
+	// One that resets everytime enter is pressed.
+
+	now := time.Now()
+	runStart = now
+	splitStart = now
+
+	totalDurationBox := tview.NewBox().SetDrawFunc(drawRunTime)
+	splitDurationBox := tview.NewBox().SetDrawFunc(drawSplitTime)
+
+	timerContainer := tview.NewFlex().SetFullScreen(true).
+		AddItem(totalDurationBox, 0, 1, false).
+		AddItem(splitDurationBox, 0, 1, false)
+
 	go refresh()
+	if err := app.SetRoot(timerContainer, true).SetInputCapture(inputHandler).Run(); err != nil {
+		return err
+	}
+
 	return nil
 }
